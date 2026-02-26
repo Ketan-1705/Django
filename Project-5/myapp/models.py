78f9a4bfb7d67c06fd0fdee4b53abef3b6dede37
goswami22 <ketangoswami1705@gmail.com>
2026-02-26 17:19:52 +0530

    26/02/2026

--- Project-5/myapp/models.py
+++ Project-5/myapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    fname = models.CharField(max_length=100)
+    lname = models.CharField(max_length=100)
+    address = models.CharField(max_length=200)
+    phone=models.PositiveBigIntegerField()
+    email = models.EmailField()
+    password = models.CharField(max_length=100)
+    profile_pic = models.ImageField(upload_to='profile_pics/')
+    usertype=models.CharField(max_length=100,default='buyer')
+
+    def __str__(self):
+        return self.fname + ' ' + self.lname
--- Project-5/myapp/views.py
+++ Project-5/myapp/views.py
@@ -1,13 +1,112 @@
 from django.shortcuts import render
+from .models import User
 
 # Create your views here.
 def index(request):
     return render(request,'index.html')
 
 def login(request):
-    return render(request,'login.html')
-
+    if request.method =='POST':
+        try:
+            user=User.objects.get(email=request.POST['email'])
+            if user.password==request.POST['password']:
+                request.session['email']=user.email
+                request.session['name']=user.fname
+                request.session['profile_pic']=user.profile_pic.url
+                if user.usertype=="buyer":
+                    return render(request,'index.html')
+                else:
+                    return render(request,'seller_index.html',{'user':user})
+            else:
+                msg='Invalid password'
+                return render(request,'login.html',{'msg':msg})
+        except Exception as e:
+            print(e)
+            msg='Email Not registered'
+            return render(request,'login.html',{'msg':msg})
+    else:
+        return render(request,'login.html')
 def signup(request):
-    return render(request,'signup.html')
+    if request.method == 'POST':
+        try:
+            User.objects.get(email=request.POST['email'])
+            msg='Email already exists'
+            return render(request,'signup.html',{'msg':msg})
+        except:
+            if request.POST['password']==request.POST['c_password']:
+                User.objects.create(
+                    fname=request.POST['fname'],
+                    lname=request.POST['lname'],
+                    address=request.POST['address'],
+                    phone=request.POST['phone'],
+                    email=request.POST['email'],
+                    password=request.POST['password'],
+                    profile_pic=request.FILES['profile_pic'],
+                    usertype=request.POST['user_type'],
+                )
+                msg="signup successful"
+                return render(request,'login.html',{'msg':msg})
+            else:
+                msg="password and confirm password does not match"
+                return render(request,'signup.html',{'msg':msg})
+    else:
+        return render(request,'signup.html')
+def logout(request):
+    try:
+        del request.session['email']
+        del request.session['name']
+        del request.session['profile_pic']
+        msg='Logout successful'
+    except:
+        msg='Logout successful'
+    return render(request,'login.html',{'msg':msg})
 def contact(request):
-    return render(request,'contact.html')
+    return render(request,'contact.html',)
+def forgot_password(request):
+    return render(request,'forgot_password.html',)
+def profile(request):
+    user=User.objects.get(email=request.session['email'])
+    if request.method=='POST':
+        user.fname=request.POST['fname']
+        user.lname=request.POST['lname']
+        user.address=request.POST['address']   
+        user.phone=request.POST['phone']
+        try:
+            user.profile_pic=request.FILES['profile_pic']
+        except:
+            pass
+        user.save()
+        request.session['profile_pic']=user.profile_pic.url
+        msg="Profile updated successfully"
+        return render(request,'profile.html',{'user':user,'msg':msg})
+    else:
+        return render(request,'profile.html',{'user':user})
+def otp(request):
+    return render(request,'otp.html',)
+def change_password(request):
+    return render(request,'change_password.html',)
+def seller_index(request):
+    return render(request,'seller_index.html',)
+def add_product(request):
+    return render(request,'add_product.html',)
+def products(request):
+    return render(request,'products.html',)
+def orders(request):
+    return render(request,'orders.html',)
+def seller_profile(request):
+    user=User.objects.get(email=request.session['email'])
+    if request.method=='POST':
+        user.fname=request.POST['fname']
+        user.lname=request.POST['lname']
+        user.address=request.POST['address']   
+        user.phone=request.POST['phone']
+        try:
+            user.profile_pic=request.FILES['profile_pic']
+        except:
+            pass
+        user.save()
+        request.session['profile_pic']=user.profile_pic.url
+        msg="Profile updated successfully"
+        return render(request,'seller_profile.html',{'user':user,'msg':msg})
+    else:
+        return render(request,'seller_profile.html',{'user':user})
